bd566ac1538a81bb260cae6fb5fd083bbec88a5b
pratham <prathambattamwar1@gmail.com>
2025-03-19 23:55:12 +0530

    dfgedg

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -34,8 +34,6 @@ const Header = () => {
               aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
-              <span className="navbar-toggler-icon"></span>
-              <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
